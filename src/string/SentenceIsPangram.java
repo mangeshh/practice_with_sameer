6c03df59a3237993b603aca41df181306f1cba4e
mangeshhankare <mangesh.hankare@gmail.com>
2023-07-01 08:43:27 -0400

    added all practice sessions with Sameer.

--- src/string/SentenceIsPangram.java
+++ src/string/SentenceIsPangram.java
@@ -11,6 +11,7 @@ public class SentenceIsPangram {
         char [] chars = sentence.toCharArray();
         int[] charIndexes = new int[26];
         for(int i=0; i < chars.length; i++){
+            // lower case 'a' starts from 97 so subtracting 97 to make index 0 (starting point).
             charIndexes[(int) chars[i]-97] = 1;
         }
         for(int j=0; j < charIndexes.length; j++){
