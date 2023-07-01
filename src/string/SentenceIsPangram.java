78f465ef76a97740281338456517f11785d74ce6
mangeshhankare <mangesh.hankare@gmail.com>
2023-07-01 08:42:37 -0400

    added all practice sessions with Sameer.

---
+++ src/string/SentenceIsPangram.java
@@ -0,0 +1,30 @@
+package string;
+
+
+/**
+ * https://leetcode.com/problems/check-if-the-sentence-is-pangram/description/
+ */
+public class SentenceIsPangram {
+
+    public static boolean checkIfPangram(String sentence) {
+
+        char [] chars = sentence.toCharArray();
+        int[] charIndexes = new int[26];
+        for(int i=0; i < chars.length; i++){
+            charIndexes[(int) chars[i]-97] = 1;
+        }
+        for(int j=0; j < charIndexes.length; j++){
+            if(charIndexes[j] < 1){
+
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String sentence = "thequickbrownfoxjumpsoverthelazydog";
+        System.out.println(checkIfPangram(sentence));
+    }
+}
