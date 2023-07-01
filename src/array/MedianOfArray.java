86045fd7b687b89f645e016a627e5b83a2fe201b
mangeshhankare <mangesh.hankare@gmail.com>
2023-07-01 09:54:09 -0400

    added all practice sessions with Sameer.

---
+++ src/array/MedianOfArray.java
@@ -0,0 +1,47 @@
+package array;
+
+/**
+ *  https://leetcode.com/problems/median-of-two-sorted-arrays/description/
+ */
+public class MedianOfArray {
+
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+        int newLength = nums1.length + nums2.length;
+        int[] mergedArray = new int[newLength];
+        int k = 0;
+        int i = 0;
+        int j = 0;
+        Double answer = null;
+
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                mergedArray[k++] = nums1[i++];
+            } else {
+                mergedArray[k++] = nums2[j++];
+            }
+        }
+
+        while (i < nums1.length) {
+            mergedArray[k++] = nums1[i++];
+        }
+
+        while (j < nums2.length) {
+            mergedArray[k++] = nums2[j++];
+        }
+
+
+        if (mergedArray.length % 2 == 0) {
+            answer = (double) (mergedArray[(newLength / 2 - 1)] + mergedArray[(newLength / 2)]) / 2;
+        } else {
+            answer = (double) mergedArray[(newLength / 2)];
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findMedianSortedArrays(new int[]{1,3,5}, new int[]{2,3}));
+    }
+}
+
---
+++ src/array/MergeSortedArrays.java
@@ -0,0 +1,38 @@
+package array;
+
+
+import java.util.Arrays;
+
+public class MergeSortedArrays {
+
+    public static int[] sortArrays(int[] nums1, int[] nums2) {
+
+        int newLength = nums1.length + nums2.length;
+        int[] mergedArray = new int[newLength];
+        int k = 0;
+        int i = 0;
+        int j = 0;
+
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                mergedArray[k++] = nums1[i++];
+            } else {
+                mergedArray[k++] = nums2[j++];
+            }
+        }
+
+        while (i < nums1.length) {
+            mergedArray[k++] = nums1[i++];
+        }
+
+        while (j < nums2.length) {
+            mergedArray[k++] = nums2[j++];
+        }
+
+        return mergedArray;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(sortArrays(new int[]{1,3,5}, new int[]{2,3})));
+    }
+}
