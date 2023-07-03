9063411a8bcedda177d3e0c80133c8f617a6bf31
mangeshhankare <mangesh.hankare@gmail.com>
2023-07-03 09:26:18 -0400

    added all practice sessions with Sameer.

--- src/auxilary_buffer/PrintCombo.java
+++ src/auxilary_buffer/PrintCombo.java
@@ -1,8 +1,30 @@
 package auxilary_buffer;
 
+import java.util.Arrays;
+
 /**
  * Interview camp
  */
 //TODO
 public class PrintCombo {
+
+    public static void print(String space, int [] array, int [] buffer, int startIndex, int bufferIndex){
+        //System.out.print(space);
+
+        // base case.
+        if(buffer.length == bufferIndex){
+            System.out.println(Arrays.toString(buffer));
+            return;
+        }
+
+        for(int i = startIndex; i < array.length; i++){
+            buffer[bufferIndex] = array[i];
+            print("-"+space,array, buffer, i + 1, bufferIndex+1);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        print("",new int[]{1,2,3,4,5,6,7,8,9}, new int[3], 0,0);
+    }
 }
