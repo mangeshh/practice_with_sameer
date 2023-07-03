3bbe0c86204ee26bee9db46902169f1bd0105955
mangeshhankare <mangesh.hankare@gmail.com>
2023-07-03 10:09:56 -0400

    added all practice sessions with Sameer.

--- src/greedy/BuySellStock.java
+++ src/greedy/BuySellStock.java
@@ -8,10 +8,8 @@ public class BuySellStock {
         int max_profit = 0;
 
         for(int day = 1; day < prices.length; ++day){
-
             max_profit = Math.max(max_profit, prices[day] - min_price);
             min_price = Math.min(min_price, prices[day]);
-
         }
 
         System.out.println("Mx profit " + max_profit);
