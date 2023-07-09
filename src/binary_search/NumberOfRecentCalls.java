9a5890ed4ecad2cb569cea9390eff4df4b084519
mangeshhankare <mangesh.hankare@gmail.com>
2023-07-08 20:32:06 -0400

    refactored.

--- src/binary_search/NumberOfRecentCalls.java
+++ src/binary_search/NumberOfRecentCalls.java
@@ -41,9 +41,4 @@ public class NumberOfRecentCalls {
     }
 }
 
-/**
- * Your RecentCounter object will be instantiated and called as such:
- * RecentCounter obj = new RecentCounter();
- * int param_1 = obj.ping(t);
- */
 
