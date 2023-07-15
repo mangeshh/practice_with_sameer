6d70f394832a234f5e9b7a1464d17c6c82f4991c
mangeshh <mangesh.hankare@gmail.com>
2023-07-15 11:27:58 -0400

    refactored.

--- src/dynamic_programming/HouseRobber.java
+++ src/dynamic_programming/HouseRobber.java
@@ -6,6 +6,8 @@ public class HouseRobber {
     public static void main1(String[] args) {
 //        0 1 2
 //        nums: [3 5 1 7 4]
+
+//  few possibilities to rob.
 //        3 1 4 = 8
 //        5 7   = 12
 //        5 4   = 9
