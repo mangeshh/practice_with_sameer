766628d279aeab875d1066d076b97cf789f56ecf
mangeshh <mangesh.hankare@gmail.com>
2023-07-15 13:28:41 -0400

    refactored.

--- src/dynamic_programming/LongestIncreasingSubsequence.java
+++ src/dynamic_programming/LongestIncreasingSubsequence.java
@@ -6,31 +6,28 @@ import java.util.Arrays;
 public class LongestIncreasingSubsequence {
 //    3, 5, 2, 8, 4, 1
 
-//    hello -> eo
-
 //    Few valid increasing subsequence are:
 //          3, 5
 //          3
 //          5, 8
 
-//    Step: 1
+//    "definition(super imp)" - dp[i] is the longest increasing subsequence till index i [and including it]
+
 //    Base Cases:
 //    dp[0] = 1
 //    dp[1] = 2
 //    dp[2] = 1
 
-//    dp[i] is the longest increasing subsequence till index i [and including it]
-
 //    dp[3] =
 //          Choices:
 //              If nums[0] < nums[3]
-//              a. 0th index is the second last element (dp[3] = dp[0] + 1 = 2)     [3, 8]
+//              a. 0th index is the second last element (dp[3] = dp[0] + 1 = 2)      [3, 8]
 
-    //          If nums[1] < nums[3]
+//              If nums[1] < nums[3]
 //              b. 1st index is the second last element (dp[3] = dp[1] + 1 = 3)      [3, 5, 8]
 
 //              If nums[2] < nums[3]
-//              c. 2nd index is the seconf last element (dp[3] = dp[2] + 1 = 2)       [2, 8]
+//              c. 2nd index is the second last element (dp[3] = dp[2] + 1 = 2)      [2, 8]
 
 
     public static void main(String[] args) {
@@ -45,7 +42,7 @@ public class LongestIncreasingSubsequence {
         dp[1] = (nums[1] > nums[0] ? 2 : 1);
 
         for(int largestIndex = 2; largestIndex < n; ++largestIndex){
-
+            dp[largestIndex] = 1;
             for(int secondLargestIndex = 0; secondLargestIndex < largestIndex; ++secondLargestIndex){
                 if(nums[secondLargestIndex] < nums[largestIndex]){
                     dp[largestIndex] = Math.max(dp[largestIndex], dp[secondLargestIndex] + 1);
@@ -53,6 +50,7 @@ public class LongestIncreasingSubsequence {
             }
         }
 
+        System.out.println(Arrays.toString(dp));
 
         Arrays.sort(dp);
         System.out.println("The longest increasing subsequence in this array " + dp[n - 1]);
