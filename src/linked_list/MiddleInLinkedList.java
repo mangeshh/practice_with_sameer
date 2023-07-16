fa04ca8cf71988de9fca19db5c1191180ebf7aac
mangeshh <mangesh.hankare@gmail.com>
2023-07-16 00:00:15 -0400

    refactored.

--- src/linked_list/MiddleInLinkedList.java
+++ src/linked_list/MiddleInLinkedList.java
@@ -5,17 +5,6 @@ class ListNode {
     int val;
     ListNode next;
 
-    ListNode() {
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
 }
 
 public class MiddleInLinkedList {
