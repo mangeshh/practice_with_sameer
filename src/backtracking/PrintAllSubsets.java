33f6c8174f0b7238e027bade2b94fbf17c66c5b8
mangeshh <mangesh.hankare@gmail.com>
2023-07-19 23:53:12 -0400

    refactored.

--- src/backtracking/PrintAllSubsets.java
+++ src/backtracking/PrintAllSubsets.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 public class PrintAllSubsets {
 
-
     public static List<List<Integer>> findSubsets(int[] nums) {
         List<List<Integer>> subsets = new ArrayList<>();
         backtrack(0, new ArrayList<>(), subsets, nums);
@@ -14,25 +13,28 @@ public class PrintAllSubsets {
 
     //    This will generate all subsets with elements starting from currentIndex
     public static void backtrack(int currentIndex, ArrayList<Integer> previousSubset, List<List<Integer>> subsets, int[] nums) {
-
+        System.out.println("backtrack(" + currentIndex + ", " + previousSubset + ")" );
         if (currentIndex == nums.length) {
+            System.out.println("                             Adding " + previousSubset);
             subsets.add(new ArrayList<>(previousSubset));
+            System.out.println("based condition matched so returning!!!!");
             return;
         }
 
-        previousSubset.add(nums[currentIndex]);
+        previousSubset.add(nums[currentIndex]); // 1 ; 2; 4; 8
         backtrack(currentIndex + 1, previousSubset, subsets, nums);
 
-        previousSubset.remove(previousSubset.size() - 1);
-        backtrack(currentIndex + 1, previousSubset, subsets, nums);
+        int removedElement = previousSubset.remove(previousSubset.size() - 1); // remove 8
+        System.out.println("                             Removing from previousSubset " + removedElement);
+        backtrack(currentIndex + 1, previousSubset, subsets, nums); // 4, (1,2,4), (ans, (input);
 
     }
 
     public static void main(String[] args) {
-        int[] nums = new int[]{10, 5, 4, 9};
+        int[] nums = new int[]{1, 2, 4, 8};
 
         List<List<Integer>> subsets = findSubsets(nums);
-        System.out.println(subsets);
+        System.out.println("answer is " + subsets);
     }
 }
 
---
+++ src/backtracking/readMe.md
@@ -0,0 +1,6 @@
+consider all subset : all combinations. Say we have 3 elements
+in array {1,5,6} then 2 raised to 3, meaning 8 possibilities.
+
+Note - we have to use preorder traversal, only.
+
+![img.png](img.png)
