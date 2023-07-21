778dbd1039eae8c3f891b1eec9b16096dc8938dc
mangeshh <mangesh.hankare@gmail.com>
2023-07-20 23:48:05 -0400

    refactored.

---
+++ src/dynamic_programming/PascalTriangle.java
@@ -0,0 +1,36 @@
+package dynamic_programming;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalTriangle {
+
+    public static List<List<Integer>> generate(int numRows) {
+
+        List<List<Integer>> rows = new ArrayList(numRows);
+
+        for (int rowIndex = 0; rowIndex < numRows; rowIndex++) {
+
+            List<Integer> currentRow = new ArrayList(rowIndex + 1);
+            currentRow.add(0, 1);
+            for (int columnIndex = 1; columnIndex < rowIndex; columnIndex++) {
+                List<Integer> previousRow = rows.get(rowIndex - 1);
+                currentRow.add(previousRow.get(columnIndex - 1) + previousRow.get(columnIndex));
+            }
+
+            if(rowIndex > 0){
+                currentRow.add(1);
+            }
+            rows.add(currentRow);
+        }
+
+        System.out.println(rows);
+        return rows;
+    }
+
+
+    public static void main(String[] args) {
+        generate(5);
+    }
+}
+
