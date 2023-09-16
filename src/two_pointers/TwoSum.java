3377a04c4a2af280256745acf3659c6d71b25660
mangeshh <mangesh.hankare@gmail.com>
2023-09-16 09:42:28 -0400

    refactored.

--- src/two_pointers/TwoSum.java
+++ src/two_pointers/TwoSum.java
@@ -1,9 +1,31 @@
 package two_pointers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 public class TwoSum {
-    public int[] twoSum(int[] nums, int target) {
+
+    public static List<int[]> allTwoSum(int[] nums, int target) {
+        List<int[]> list = new ArrayList<>();
+        HashMap<Integer, Integer> freq = new HashMap <>();
+
+        for(int i = 0; i < nums.length; ++i){
+            int firstNumber = nums[i];
+            int secondNumber = target - nums[i];
+
+            if(freq.containsKey(secondNumber)){
+                list.add(new int[]{i, freq.get(secondNumber)});
+            }
+
+            freq.put(firstNumber, freq.getOrDefault(firstNumber, 0) + 1);
+        }
+
+        return list;
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
         // [key, value]
         // nums[index] = value;
 
@@ -27,5 +49,14 @@ public class TwoSum {
         }
 
         return new int[] {};
+    }
+
+    public static void main(String[] args) {
+        // int [] answer = twoSum(new int[]{1,2,8,10,4,6,6,6}, 12);
+        //System.out.println(Arrays.toString(answer));
+        List<int []> answerList = allTwoSum(new int[]{1,2,8,10,4,6,6,6}, 12);
+        for(int [] i : answerList){
+            System.out.println(Arrays.toString(i));
+        }
     }
 }
