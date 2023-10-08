69897ff59e18aafeab953598f270ce83ae4421c2
mangeshh <mangesh.hankare@gmail.com>
2023-10-08 08:23:07 -0400

    adding lambda (stream)

--- src/backtracking/SortedArrayToBinarySearchTree.java
+++ src/backtracking/SortedArrayToBinarySearchTree.java
@@ -1,20 +1,22 @@
 package backtracking;
 
 
-class Node {
-    int val;
-    Node left;
-    Node right;
 
-    public Node(int val){
-        this.val = val;
-        this.left = null;
-        this.right = null;
-    }
-
-}
 
 public class SortedArrayToBinarySearchTree {
+
+    static class Node {
+        int val;
+        Node left;
+        Node right;
+
+        public Node(int val){
+            this.val = val;
+            this.left = null;
+            this.right = null;
+        }
+
+    }
 
     public static Node createBSTree(int [] nums, int start, int end){
         if(start > end){
