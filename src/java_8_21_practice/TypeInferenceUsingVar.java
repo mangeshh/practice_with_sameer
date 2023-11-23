d432499833af4bc9956b013ee94ccc648dce2192
mangeshh <mangesh.hankare@gmail.com>
2023-11-23 11:40:52 -0500

    refactoring

--- src/java_8_21_practice/TypeInferenceUsingVar.java
+++ src/java_8_21_practice/TypeInferenceUsingVar.java
@@ -1,46 +1,60 @@
 package java_8_21_practice;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+class Employee {
+    String name;
+    double salary;
+
+}
+
 public class TypeInferenceUsingVar {
 
-/*
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
 
-1> List<String> list = new ArrayList<>();
+        List<String> list = new ArrayList<>();
 
-2> Predicate<Employee> highSalary = (Employee e) -> e.salary > 1_00_000 ;
-   // Type Inference, looking at left-hand side.
-   Predicate<Employee> highSalary = e -> e.salary > 1_00_000 ;
+        Predicate<Employee> highSalary1 = (Employee e) -> e.salary > 1_00_000;
+        // Type Inference, looking at left-hand side.
+        Predicate<Employee> highSalary2 = e -> e.salary > 1_00_000;
 
-3> String ram = "Ram";
-   // Type Inference, looking at right-hand side.
-   var ram = "ram"
+        // String ram = "Ram";
+        // Type Inference, looking at right-hand side.
+        var ram = "ram"; // it will be defined as String.
 
-4> Employee e = new Employee();
-   // Type Inference, looking at right-hand side. (bytecode is same, nothing changes at runtime!)
-   var e = new Employee();
+        // Employee e = new Employee();
+        // Type Inference, looking at right-hand side. (bytecode is same, nothing changes at runtime!)
+        var e = new Employee(); // it will be defined as Employee.
 
-5> var list = List.of(1,2,3,4);
-   for (var i : list){
-      print(i);
-   }
+        var listVar = List.of(1, 2, 3, 4);
+        for (var i : listVar) {
+            System.out.println(i);
+        }
 
-6> NOTE = var can not be used, as `method argument` (We will not know type to pass
-   looking at the java docs, `return type` (developers will keep changing return type and
-   it will cause type incompatibility) and `class instance variables` (reflection issues)
-   var can not be used in `construction parameter` and `catch block` catch (var fileNotFoundException)
+        /**
+         *
+        NOTE = var can not be used, as `method argument` (We will not know type to pass
+        looking at the java docs, `return type` (developers will keep changing return type and
+        it will cause type incompatibility) and `class instance variables` (reflection issues)
+        var can not be used in `construction parameter` and `catch block` catch (var fileNotFoundException)
 
-7> var `has to be initialized` while declaration.
+        var `has to be initialized` while declaration.
 
-8> confusion,
-   byte b = 0; // it will narrow down number zero to byte but if we use var then
-   var b = 0 // it will be declared as int type.
+        **/
 
-9> if we write
-   var list = new ArrayList<>();
-   list.add(1);
-   list.add("one");
-   // here we can add string and int because now because of var that list is 'list of object'.
-   Note - but it's good if we do,  var list = new ArrayList<String>();
+        // confusion!
+        //byte b = 0; it will narrow down number zero to byte but if we use var then
+        var b = 0; // it will be declared as int type.
 
-*/
+        var listWithDiffDataTypes = new ArrayList<>();
+        listWithDiffDataTypes.add(1);
+        listWithDiffDataTypes.add("one");
+        // here we can add string and int because now because of var that list is 'list of object'.
+        // but it's good if we do, var list = new ArrayList<String>();
+
+    }
 
 }
