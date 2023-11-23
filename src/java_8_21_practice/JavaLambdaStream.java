ffb96cf26bc30402936c02321d6ad2aef41872e8
mangeshh <mangesh.hankare@gmail.com>
2023-11-23 12:20:04 -0500

    refactoring

--- src/java_8_21_practice/JavaLambdaStream.java
+++ src/java_8_21_practice/JavaLambdaStream.java
@@ -7,6 +7,8 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
+import static java_8_21_practice.Util.*;
+
 
 /**
  * ===============================================================================================================================
@@ -1127,17 +1129,4 @@ public class JavaLambdaStream {
         return "test-value";
     }
 
-    public static String getCurrentMethodName() {
-        // Get the stack trace
-        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-
-        // The first element in the stack trace is this method
-        // The second element is the caller's method
-        if (stackTrace.length >= 3) {
-            StackTraceElement caller = stackTrace[2]; // Index 2 corresponds to the caller's method
-            return caller.getMethodName();
-        } else {
-            return "Unknown"; // Handle the case when method name cannot be determined
-        }
-    }
 }
--- src/java_8_21_practice/PatternMatching.java
+++ src/java_8_21_practice/PatternMatching.java
@@ -5,6 +5,8 @@ import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Optional;
 
+import static java_8_21_practice.Util.*;
+
 public class PatternMatching {
 
     public static void exampleWithSwitchYield() {
@@ -194,17 +196,4 @@ public class PatternMatching {
         }
     }
 
-    public static String getCurrentMethodName() {
-        // Get the stack trace
-        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-
-        // The first element in the stack trace is this method
-        // The second element is the caller's method
-        if (stackTrace.length >= 3) {
-            StackTraceElement caller = stackTrace[2]; // Index 2 corresponds to the caller's method
-            return caller.getMethodName();
-        } else {
-            return "Unknown"; // Handle the case when method name cannot be determined
-        }
-    }
 }
